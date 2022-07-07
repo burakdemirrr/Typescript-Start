d5fcb98bf667d67a396162fc91a37a59b5e125ef
Burak <mburak.demir.059@gmail.com>
2022-07-07 14:42:57 +0300

    TypeScript Basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name:string="burak";
+
+let surname:any;
+
+
+let age:number | string=5;
+
+age="burka"
+age=5;
+
+let printName:(name:string)=>void;
+
+let personName:unknown; 
+
+//void returns undefined never returns nothing
+
+let isStudent:boolean=false;
+
+let hobbies:string[]=["sport","reading"];
+
+let role:[number,string]=[2,"author"];
+
+type Person={
+  name:string;
+  //if ? is used,then it is optional
+  age?:number;
+}
+let person:Person={name:"burak",age:5};
+
+
+interface Persona{
+  name:string;
+  age?:number
+}
+interface Guy extends Person{
+  profession:string;
+}
+class name
+
+
+type X={
+  a:string;
+  b:number;
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{name}</h1>
     </div>
   );
 }
