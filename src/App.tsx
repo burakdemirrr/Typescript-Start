53731322ec6a31439cf7eca59077038af82bed89
Burak <mburak.demir.059@gmail.com>
2022-07-07 18:31:08 +0300

    TodoLİst

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
 
-let name:string="burak";
+let name: string = "burak";
 
-let surname:any;
+let surname: any;
 
 
-let age:number | string=5;
+let age: number | string = 5;
 
-age="burka"
-age=5;
+age = "burka"
+age = 5;
 
-let printName:(name:string)=>void;
+let printName: (name: string) => void;
 
-let personName:unknown; 
+let personName: unknown;
 
 //void returns undefined never returns nothing
 
-let isStudent:boolean=false;
+let isStudent: boolean = false;
 
-let hobbies:string[]=["sport","reading"];
+let hobbies: string[] = ["sport", "reading"];
 
-let role:[number,string]=[2,"author"];
+let role: [number, string] = [2, "author"];
 
-type Person={
-  name:string;
+type Person = {
+  name: string;
   //if ? is used,then it is optional
-  age?:number;
+  age?: number;
 }
-let person:Person={name:"burak",age:5};
+let person: Person = { name: "burak", age: 5 };
 
 
-interface Persona{
-  name:string;
-  age?:number
+interface Persona {
+  name: string;
+  age?: number
 }
-interface Guy extends Person{
-  profession:string;
-}
-
-type X={
-  a:string;
-  b:number;
+interface Guy extends Person {
+  profession: string;
 }
 
+type X = {
+  a: string;
+  b: number;
+}
 
-const App:React.FC=()=> {
+
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }])
+      setTodo("");
+    }
+
+  }
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList  todos={todos} setTodos={setTodos}/>
+      {
+        todos.map((t)=>(
+          <li>{t.todo}</li>
+        ))
+      }
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./styles.css"
-const InputField = () => {
+
+interface Props{
+  todo:string;
+  setTodo:React.Dispatch<React.SetStateAction<string>>;
+  handleAdd:(e:React.FormEvent)=>void
+}
+
+
+
+const InputField: React.FC<Props> = ({todo,setTodo,handleAdd}:Props) => {
   return (
-    <form className='input'>
-        <input type="input" placeholder='Enter a Task'  className='input__box'/>
-        <button className='input__submit' type='submit'>Go</button>
+    <form className='input' onSubmit={(e)=>handleAdd(e)}>
+        <input type="input" placeholder='Enter a Task' value={todo} onChange={(e)=>setTodo(e.target.value)} className='input__box'/>
+        <button className='input__submit' type='submit' >Go</button>
 
     </form>
   )
